3dabcbbb844c41210fd1b8907a7807e69d2c68c0
Moon-Truther <65514148+Moon-Truther@users.noreply.github.com>
2020-05-27 14:55:33 +1200

    finished video 5 (help box close button)

--- Video 4.5.py
+++ Video 4.5.py
@@ -33,7 +33,7 @@ class Converter:
         # Text to show the Help button was pressed
         print("Ha shame")
         get_help = Help(self)
-        get_help.help_text.configure(text="Google it idk lol")
+        get_help.help_text.configure(text="Situation 1: Google it idk lol \nSituation 2: Die \nSituation 3: Turn it off and on again")
         
 
 class Help:
@@ -46,26 +46,33 @@ class Help:
         # creates actual box
         self.help_box = Toplevel()
         
+        # Re-enable help button when red cross is used
+        self.help_box.protocol("WM_DELETE_WINDOW", partial(self.close_help, partner))
         
         # Gui Frame
-        self.help_box_frame = Frame(self.help_box,height=300,width=500,bg=background_c)
+        self.help_box_frame = Frame(self.help_box,height=100,width=300,bg=background_c)
         self.help_box_frame.grid()
         
         # Help box Heading (row 0)
-        self.help_box_label = Label(self.help_box,text="H E L P",
+        self.help_box_label = Label(self.help_box_frame,text="H E L P",
                                    font=("Arial","20","italic"),
                                    bg=background_c,padx=10,pady=10)
         
         self.help_box_label.grid(row=0)
         
         # Help text (label, row 1)
-        self.help_text = Label(self.help_box, text="", justify=LEFT,
+        self.help_text = Label(self.help_box_frame, text="", justify=LEFT,
                                width=40, bg= background_c, wrap=250)
-        self.help_text.grid(column=0,row=1)
+        self.help_text.grid(row=1)
         
         # Closing button (row 2)
-        self.help_close_button = Button(self.help_box, text="CLOSE",padx=10,pady=10)
-        self.help_close_button.grid(row=2)        
+        self.help_close_button = Button(self.help_box_frame, text="CLOSE",padx=10,pady=10,
+                                        command=partial(self.close_help, partner))
+        self.help_close_button.grid(row=2,pady=10)
+    
+    def close_help(self, partner):
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 #main routine
 if __name__ == "__main__":
